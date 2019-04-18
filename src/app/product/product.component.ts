c3ee57dd65a48f3099ae7044e84b5c4aa4451c49
michael <glacieraaa@gmail.com>
2019-04-18 14:49:16 -0400

    add product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  private products: Product[];
+
   constructor() { }
 
   ngOnInit() {
+    //console.log(this.products[0].id);
+    this.products = [
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+       new Product(1, 'Asus Rog', 'https://www.notebookcheck.net/fileadmin/Notebooks/Asus/GL704GM-DH74/gl704gm.jpg', 2300, 'Boog game laptop', 4, ['laptop', 'gaming']),
+
+    ];
+    console.log(this.products[0].id);
   }
 
 }
+
+
+
---
+++ src/app/product/product.model.ts
@@ -0,0 +1,21 @@
+
+
+export class Product {
+
+  // public id: number;
+  // public  name: string;
+  // public  image: string;
+  // public  price: number;
+  // public  description: string;
+  // public  star: number;
+  // public  category: string[];
+
+  constructor(
+    public id: number,
+    public name: string,
+    public image: string,
+    public price: number,
+    public description: string,
+    public star: number,
+    public category: string[]) {}
+}
