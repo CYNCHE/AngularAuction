f1d7939515885c837a4945f4293f9012e3073624
michael <glacieraaa@gmail.com>
2019-04-20 23:49:36 -0400

    add comment model

---
+++ src/app/product/comment.model.ts
@@ -0,0 +1,11 @@
+export class Comment {
+
+  constructor(public id: number,
+              public productId: number,
+              public timeStamp: string,
+              public user: string,
+              public rating: number,
+              public content: string) {
+
+  }
+}
