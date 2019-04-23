1a4b4b13872ad07893395cee87fc55b80cfe290b
michael <glacieraaa@gmail.com>
2019-04-23 11:37:33 -0400

    complete star component

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Product } from '../product/product.model';
 import { ProductService } from '../shared/product.service';
 import { Comment } from '../product/comment.model';
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-product-detail',
@@ -16,6 +17,10 @@ export class ProductDetailComponent implements OnInit {
   newRating: number = 5;
   newComment: string = "";
 
+  faEdit = faEdit;
+
+  public isCommentShown: boolean = false;
+
   comments: Comment[];
   constructor(private route: ActivatedRoute,
               private productService: ProductService) { }
@@ -27,8 +32,18 @@ export class ProductDetailComponent implements OnInit {
   }
 
   addComment() {
+    console.log(this.newRating);
     let comment = new Comment(1, this.product.id, new Date().toDateString(), "Vanessa", this.newRating, this.newComment);
     this.comments.unshift(comment);
+
+    // update product rate after each comment submission
+    let sum =  this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rate = sum / this.comments.length;
+
+    // reset to default
+    this.newRating = 5;
+    this.newComment = "";
+    this.isCommentShown = false;
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { faStarHalfAlt, faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
 
@@ -7,15 +7,15 @@ import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input() rate: number;
-  public rateChange: EventEmitter<number> = new EventEmitter();
+  @Output() public rateChange: EventEmitter<number> = new EventEmitter();
   // by deafault review contains five empty stars
   public stars: number[] = [0, 0, 0, 0, 0];
 
 
-  public readonly: boolean = true;
+  @Input() public readonly: boolean = true;
 
   faStarHalf = faStarHalfAlt;
   faStar = faStar;
@@ -28,6 +28,16 @@ export class StarsComponent implements OnInit {
     // decide how many full stars and half star
     // use number to indicate the shape of star
     // full star code 1, half star code 2
+    let fullStar = Math.floor(this.rate), halfStar = this.rate % 1;
+    let i = 0;
+    for (; i < fullStar; ++i) this.stars[i] = 1;
+    if (halfStar > 0 && halfStar < 1) this.stars[i] = 2;
+    for (; i < 5; ++i) this.stars[i] = 0;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
     let fullStar = Math.floor(this.rate), halfStar = this.rate % 1;
     let i = 0;
     for (; i < fullStar; ++i) this.stars[i] = 1;
