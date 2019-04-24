bc8e86999c7503ef4456cdd20d2c7cd01d35da62
michael <glacieraaa@gmail.com>
2019-04-24 01:22:28 -0400

    finished validation of the search form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-search',
@@ -10,17 +11,21 @@ export class SearchComponent implements OnInit {
 
   formModel: FormGroup;
 
-  constructor() {
+  categories: string[];
+
+  constructor(private productService: ProductService) {
     // build form data model using reactive approach
     let fb = new FormBuilder();
     this.formModel = fb.group({
-      name: ['', Validators.minLength(3)],
-      price: [null,],
-      category: ['-1']
+      name: ['', [Validators.minLength(3), Validators.required]],
+      price: [null, [this.positiveNumberValidator, Validators.required]],
+      categories: ['-1']
     })
   }
 
   ngOnInit() {
+    this.categories = this.productService.getAllCategories();
+    console.log(this.categories);
   }
 
   positiveNumberValidator(control: FormControl): any {
@@ -38,7 +43,7 @@ export class SearchComponent implements OnInit {
   }
 
   onSearch() {
-
+    if (this.formModel.valid) console.log(this.formModel);
   }
 
 
